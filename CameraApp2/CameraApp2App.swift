8a6372bf7ade1adf12eb94b007ef5600b060e2df
bradleyc95 <bradleycox1995@gmail.com>
2023-02-17 12:52:10 -0500

    Initial Commit

---
+++ CameraApp2/CameraApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  CameraApp2App.swift
+//  CameraApp2
+//
+//  Created by Bradley Cox on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct CameraApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
